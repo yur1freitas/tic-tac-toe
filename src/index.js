21d71cead09ceecf66b067846996eca7f8d51fcf
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2025-08-27 19:09:55 -0300

    feat: adicione a possibilidade de selecionar a dificuldade do jogo
    Anteriormente, a dificuldade do jogo já era pré-estabelecidade, não
    sendo possível selecioná-la pela interface
    
    Agora, com o reajuste do layout do jogo, foi adicionado uma opção
    que permite selecionar um dos quatros níveis de dificuldade
    disponíveis

--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@ import './lucide.js'
 
 import { GameArea } from './components/GameArea.js'
 
-const { render, nextRound, resetGame } = GameArea()
+const { render, nextRound, resetGame, updateDifficulty } = GameArea()
 
 const resetBtn = document.querySelector('.game__reset-btn')
 resetBtn.addEventListener('click', resetGame)
 
 const nextRoundBtn = document.querySelector('.game__next-round-btn')
 nextRoundBtn.addEventListener('click', nextRound)
+
+const difficultySelect = document.getElementById('difficulty')
+
+window.addEventListener('DOMContentLoaded', () => {
+    updateDifficulty(difficultySelect.value)
+})
+
+difficultySelect.addEventListener('change', (e) => {
+    updateDifficulty(e.currentTarget.value)
+})
 
 render()
